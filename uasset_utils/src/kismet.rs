4972d13d25bf76d7a04510e3f14c9b42566fa7bc
Truman Kilen <t@kilen.me>
2023-08-31 20:27:49 -0500

    broken

--- uasset_utils/src/kismet.rs
+++ uasset_utils/src/kismet.rs
@@ -1,36 +1,75 @@
+macro_rules! inst_member {
+    (@inst $name:ident) => {
+        $name: Box<Expr>,
+    };
+    (@vec $name:ident) => {
+        $name: Vec<Expr>,
+    };
+}
+
+macro_rules! inst_walk {
+    (_ $member_name:ident : Expr) => {
+        walk(ex.$member_name);
+        //walk(ex.$member_name)
+        //$name: Box<Expr>,
+    };
+    (_ $member_name:ident : Box<Expr>) => {
+        walk(ex.$member_name);
+        //walk(ex.$member_name)
+        //$name: Box<Expr>,
+    };
+    (_ $tp:ty) => {
+        //println!("fallback to type: {}", stringify!($tp));
+    };
+    ($member_name:ident : $($tp:tt)*) => {
+        inst_walk!(_ $member_name : $($tp)*)
+    };
+}
+
 macro_rules! inst {
-    ($name:ident, $( $a:tt )* ) => {
-        struct $name {
-            $( $a: i32, )*
+    ($name:ident, $( $member_name:ident: $member_type:tt ),* ) => {
+        pub struct $name {
+            $( $member_name: $member_type, )*
         }
     };
 }
 
-macro_rules! braces_unwrap {
-    ( $Callback:path, $name:ident, $( $Src : tt )* ) => {
-        $Callback!
-        (
-            $name,
-            $( $Src )*
-        );
+macro_rules! expr_enum {
+    ($name:ident, ) => {
+        $name
     };
 }
 
 macro_rules! for_each {
-    ( $( $name:ident { $( $Each : tt ),* $(,)?} ),* $(,)?) => {
-        mod a {
-            $( braces_unwrap!( inst, $name, $($Each)* );)*
+    ( $( $name:ident { $( $member_name:ident : $($member_type:tt)* ),* $(,)?} ),* $(,)?) => {
+        pub enum Expr {
+            $( $name($name), )*
         }
-        mod b {
-            $( braces_unwrap!( inst, $name, $($Each)* );)*
+        $( inst!($name, $($member_name : $member_type),* );)*
+        fn walk_macro(ex: &Expr) {
+            match ex {
+                $( Expr::$name(ex) => {
+                    //walk_macro();
+                    $(inst_walk!($member_name : $member_type);)*
+
+                }, )*
+            }
         }
     };
 }
 
 for_each!(
-    ExLocalVariable { a, b },
-    ExInstanceVariable { a },
-    ExDefaultVariable { b },
+    ExLocalVariable {
+        a: Expr,
+        a: Vec<Expr>,
+        b: Box<Expr>
+    },
+    ExInstanceVariable {
+        a: Vec<Expr>,
+    },
+    ExDefaultVariable {
+        b: Box<Expr>
+    },
 );
 
 //for_each!( ExLocalVariable, ExInstanceVariable, ExDefaultVariable,);
