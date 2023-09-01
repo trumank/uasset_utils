ad9c20578a50b5add6302309e5328ee926ba905c
Truman Kilen <t@kilen.me>
2023-08-31 23:05:33 -0500

    broken

--- uasset_utils/src/kismet.rs
+++ uasset_utils/src/kismet.rs
@@ -8,61 +8,57 @@ macro_rules! inst_member {
 }
 
 macro_rules! inst_walk {
-    (_ $member_name:ident : Expr) => {
-        walk(ex.$member_name);
-        //walk(ex.$member_name)
-        //$name: Box<Expr>,
+    (@ $ex:ident $member_name:ident : Expr) => {
+        walk_macro(&$ex.$member_name);
     };
-    (_ $member_name:ident : Box<Expr>) => {
-        walk(ex.$member_name);
-        //walk(ex.$member_name)
-        //$name: Box<Expr>,
+    (@ $ex:ident $member_name:ident : Box<Expr>) => {
+        walk_macro(&$ex.$member_name);
     };
-    (_ $tp:ty) => {
-        //println!("fallback to type: {}", stringify!($tp));
+    (@ $ex:ident $member_name:ident : Vec<Expr>) => {
+        for $ex in $ex.$member_name { walk_macro(&$ex); }
     };
-    ($member_name:ident : $($tp:tt)*) => {
-        inst_walk!(_ $member_name : $($tp)*)
+    (@ $ex:ident $member_name:ident : $tp:ty) => {
+    };
+    ($ex:ident, $member_name:ident : $($tp:tt)*) => {
+        inst_walk!(@ $ex $member_name : $($tp)*)
     };
 }
-
-macro_rules! inst {
-    ($name:ident, $( $member_name:ident: $member_type:tt ),* ) => {
-        pub struct $name {
-            $( $member_name: $member_type, )*
-        }
-    };
-}
-
-macro_rules! expr_enum {
-    ($name:ident, ) => {
-        $name
-    };
-}
-
-macro_rules! for_each {
-    ( $( $name:ident { $( $member_name:ident : $($member_type:tt)* ),* $(,)?} ),* $(,)?) => {
-        pub enum Expr {
-            $( $name($name), )*
-        }
-        $( inst!($name, $($member_name : $member_type),* );)*
+macro_rules! build_walk {
+    ($ex:ident, $( $name:ident, { $( $member_name:ident : { $($member_type:tt)* } )* } )* ) => {
         fn walk_macro(ex: &Expr) {
             match ex {
-                $( Expr::$name(ex) => {
-                    //walk_macro();
-                    $(inst_walk!($member_name : $member_type);)*
-
+                $( Expr::$name($ex) => {
+                    $(inst_walk!($ex, $member_name : $($member_type)*);)*
                 }, )*
             }
         }
     };
 }
 
-for_each!(
+macro_rules! inst {
+    ($name:ident, $( $member_name:ident: [ $($member_type:tt)* ] ),* ) => {
+        pub struct $name {
+            $( $member_name: $($member_type)*, )*
+        }
+    };
+}
+
+macro_rules! for_each2 {
+    ( $( $name:ident { $( $member_name:ident : [ $($member_type:tt)* ] )* } )* ) => {
+        pub enum Expr {
+            $( $name($name), )*
+        }
+        $( inst!($name, $($member_name : [$($member_type)*]),* );)*
+        build_walk!(ex, $( $name, { $( $member_name : { $($member_type)* } ) } )*);
+    };
+}
+
+/*
+for_each_entry!(
     ExLocalVariable {
-        a: Expr,
-        a: Vec<Expr>,
-        b: Box<Expr>
+        //a: String,
+        b: Vec<Expr>,
+        c: Box<Expr>
     },
     ExInstanceVariable {
         a: Vec<Expr>,
@@ -71,6 +67,23 @@ for_each!(
         b: Box<Expr>
     },
 );
+*/
+
+mod a {
+    for_each2!(
+        ExLocalVariable {
+            //a: [ Expr ]
+            b: [ Vec<Expr> ]
+            c: [ Box<Expr> ]
+        }
+        ExInstanceVariable {
+            a: [ Vec<Expr> ]
+        }
+        ExDefaultVariable {
+            b: [Box<Expr>]
+        }
+    );
+}
 
 //for_each!( ExLocalVariable, ExInstanceVariable, ExDefaultVariable,);
 
