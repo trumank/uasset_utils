4b2f0930b75a86e0995aa83444f31528904501f0
Truman Kilen <t@kilen.me>
2024-04-22 20:44:44 -0500

    Make pak_path_to_game_path case insensitive

--- uasset_utils/src/paths.rs
+++ uasset_utils/src/paths.rs
@@ -3,18 +3,20 @@ pub use typed_path::Utf8UnixComponent as PakPathComponent;
 pub use typed_path::Utf8UnixPath as PakPath;
 pub use typed_path::Utf8UnixPathBuf as PakPathBuf;
 
+use unicase::eq_ascii;
+
 pub fn pak_path_to_game_path<P: AsRef<PakPath>>(pak_path: P) -> Option<String> {
     let mut components = pak_path.as_ref().components();
     match components.next() {
-        Some(PakPathComponent::Normal("Engine")) => match components.next() {
-            Some(PakPathComponent::Normal("Content")) => {
+        Some(PakPathComponent::Normal(c)) if eq_ascii(c, "Engine") => match components.next() {
+            Some(PakPathComponent::Normal(c)) if eq_ascii(c, "Content") => {
                 Some(PakPath::new("/Engine").join(components.as_path()))
             }
-            Some(PakPathComponent::Normal("Plugins")) => {
+            Some(PakPathComponent::Normal(c)) if eq_ascii(c, "Plugins") => {
                 let mut last = None;
                 loop {
                     match components.next() {
-                        Some(PakPathComponent::Normal("Content")) => {
+                        Some(PakPathComponent::Normal(c)) if eq_ascii(c, "Content") => {
                             break last.map(|plugin| {
                                 PakPath::new("/").join(plugin).join(components.as_path())
                             })
@@ -29,7 +31,7 @@ pub fn pak_path_to_game_path<P: AsRef<PakPath>>(pak_path: P) -> Option<String> {
             _ => None,
         },
         Some(PakPathComponent::Normal(_)) => match components.next() {
-            Some(PakPathComponent::Normal("Content")) => {
+            Some(PakPathComponent::Normal(c)) if eq_ascii(c, "Content") => {
                 Some(PakPath::new("/Game").join(components))
             }
             _ => None,
